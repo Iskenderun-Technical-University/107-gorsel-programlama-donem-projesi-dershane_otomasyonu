4dcb2c8dd8831bd5575acf4650ec49af59d353bc
gökhanunal01 <gokhanunal.mdbf20@iste.edu.tr>
2022-11-06 14:26:55 +0300

    veli bilgilendirme ekle butonu

--- 2022-2023-gorselodev/2022-2023-gorselodev/veli_bilgiler.cs
+++ 2022-2023-gorselodev/2022-2023-gorselodev/veli_bilgiler.cs
@@ -8,9 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-namespace vtgb_otomasyon
+
+namespace _2022_2023_gorselodev
 {
-    public partial class veli_bilgileri : Form
+    public partial class veli_bilgiler : Form
     {
         SqlConnection con;
         SqlDataAdapter da;
@@ -29,7 +30,7 @@ namespace vtgb_otomasyon
             dataGridView1.DataSource = ds.Tables["veli_bilgileri"];
             con.Close();
         }
-        public veli_bilgileri()
+        public veli_bilgiler()
         {
             InitializeComponent();
             if (Class1.BaglantiDurum())
@@ -37,8 +38,8 @@ namespace vtgb_otomasyon
                 // MessageBox.Show("Bağlantı Kuruldu");
             }
         }
-        
-        private void veli_bilgileri_Load(object sender, EventArgs e)
+
+        private void veli_bilgiler_Load(object sender, EventArgs e)
         {
             string veri = "select * from ogr_bilgileri where ogr_tcno='" + ogr_bilgileri.deger + "'";
             veliogrno.Text = Convert.ToString(Class1.IdDegeri(veri));
@@ -56,31 +57,11 @@ namespace vtgb_otomasyon
             dataGridView1.Columns[7].HeaderCell.Value = "Kayıt Ücreti";
             dataGridView1.Columns[8].HeaderCell.Value = "Veli Öğrenci No";
         }
-        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
-        {
-            /*textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
-            velitcno.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
-            veliadsoyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
-            veliyak.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
-            veliadres.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
-            velitelefon.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
-            velieposta.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
-            kayitucreti.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
-            veliogrno.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
-            con = new SqlConnection(SqlCon);
-            cmd = new SqlCommand();
-            cmd.CommandText = "select * from veli_bilgileri where veli_tcno='" + velitcno.Text + "'";
-            cmd.Connection = con;
-            cmd.CommandType = CommandType.Text;
-            con.Open();
-            dr = cmd.ExecuteReader();*/
-
-        }
 
         private void btnekle_Click(object sender, EventArgs e)
         {
-            string veri = "select * from ogr_bilgileri where ogr_tcno='" +ogr_bilgileri.deger+ "'";
-            veliogrno.Text=Convert.ToString(Class1.IdDegeri(veri));
+            string veri = "select * from ogr_bilgileri where ogr_tcno='" + ogr_bilgileri.deger + "'";
+            veliogrno.Text = Convert.ToString(Class1.IdDegeri(veri));
             velitcno.Text = ogr_bilgileri.deger_1;
             veliadsoyad.Text = ogr_bilgileri.deger_2;
             string sql = "insert into veli_bilgileri(veli_tcno,veli_adsoyad,veli_yakinlik,veli_isadresi,veli_telefon,veli_mail,kayit_ucreti,ogr_no) values(@o1,@o2,@o3,@o4,@o5,@o6,@o7,@o9)";
@@ -92,38 +73,13 @@ namespace vtgb_otomasyon
             cmd.Parameters.AddWithValue("@o5", velitelefon.Text);
             cmd.Parameters.AddWithValue("@o6", velieposta.Text);
             cmd.Parameters.AddWithValue("@o7", kayitucreti.Text);
-            cmd.Parameters.AddWithValue("@o9",Class1.IdDegeri(veri));
+            cmd.Parameters.AddWithValue("@o9", Class1.IdDegeri(veri));
             deger_5 = kayitucreti.Text;
             Class1.KomutYollaParametreli(sql, cmd);
             GridDoldur();
-            hesap_bilgileri hes = new hesap_bilgileri();
-            hes.Show();
+            // hesap_bilgileri hes = new hesap_bilgileri();
+            // hes.Show();
             this.Hide();
-        }
-
-       
-
-        
-
-        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int secim = dataGridView1.SelectedCells[0].RowIndex;
-            textBox1.Text = dataGridView1.Rows[secim].Cells[0].Value.ToString();
-            velitcno.Text = dataGridView1.Rows[secim].Cells[1].Value.ToString();
-            veliadsoyad.Text = dataGridView1.Rows[secim].Cells[2].Value.ToString();
-            veliyak.Text = dataGridView1.Rows[secim].Cells[3].Value.ToString();
-            veliadres.Text = dataGridView1.Rows[secim].Cells[4].Value.ToString();
-            velitelefon.Text = dataGridView1.Rows[secim].Cells[5].Value.ToString();
-            velieposta.Text = dataGridView1.Rows[secim].Cells[6].Value.ToString();
-            kayitucreti.Text = dataGridView1.Rows[secim].Cells[7].Value.ToString();
-            veliogrno.Text = dataGridView1.Rows[secim].Cells[8].Value.ToString();
-
-        }
-
-        private void search3_TextChanged(object sender, EventArgs e)
-        {
-            string sql = "select * from hesap_bilgileri where ogr_adsoyad like '%";
-            Class1.ara(dataGridView1, search3, sql);
         }
     }
 }
