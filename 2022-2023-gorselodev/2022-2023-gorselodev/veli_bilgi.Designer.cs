cc474198e5abd2842319009507f72f78693cb7e9
gökhanunal01 <gokhanunal.mdbf20@iste.edu.tr>
2022-12-11 12:25:48 +0300

    veli bilgi formunu yükledim

---
+++ 2022-2023-gorselodev/2022-2023-gorselodev/veli_bilgi.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace _2022_2023_gorselodev
+{
+    partial class veli_bilgi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 81);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1126, 369);
+            this.panel2.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1126, 369);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1126, 81);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(133, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aranacak Metin";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(255, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // veli_bilgi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "veli_bilgi";
+            this.Text = "veli_bilgi";
+            this.Load += new System.EventHandler(this.veli_bilgi_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ 2022-2023-gorselodev/2022-2023-gorselodev/veli_bilgi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _2022_2023_gorselodev
+{
+    public partial class veli_bilgi : Form
+    {
+        public veli_bilgi()
+        {
+            InitializeComponent();
+        }
+
+        private void veli_bilgi_Load(object sender, EventArgs e)
+        {
+            sqlsorgu = "select sinav_takip.sinav_no,sinav_takip.ogr_no,sinav_takip.ogr_adsoyad,sinav_takip.turkce_neti,sinav_takip.matematik_neti,sinav_takip.geometri_neti,sinav_takip.tarih_neti,sinav_takip.felsefe_neti,sinav_takip.fizik_neti,sinav_takip.kimya_neti,sinav_takip.biyoloji_neti,sinav_takip.sayisal_puan,sinav_takip.esitagirlik_puan,sinav_takip.sozel_puan,veli_bilgileri.veli_adsoyad,veli_bilgileri.veli_telefon,veli_bilgileri.veli_mail from sinav_takip INNER JOIN veli_bilgileri ON sinav_takip.ogr_no=veli_bilgileri.ogr_no";
+            GridDoldur(sqlsorgu);
+
+        }
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+        string SqlCon = Class1.SqlCon;
+
+
+        void GridDoldur(string sql)
+        {
+            con = new SqlConnection(SqlCon);
+            da = new SqlDataAdapter(sql, con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "sinav_takip");
+
+            dataGridView1.DataSource = ds.Tables["sinav_takip"];
+            con.Close();
+        }
+        public string sqlsorgu;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                sqlsorgu = "select sinav_takip.sinav_no,sinav_takip.ogr_no,sinav_takip.ogr_adsoyad,sinav_takip.turkce_neti,sinav_takip.matematik_neti,sinav_takip.geometri_neti,sinav_takip.tarih_neti,sinav_takip.felsefe_neti,sinav_takip.fizik_neti,sinav_takip.kimya_neti,sinav_takip.biyoloji_neti,sinav_takip.sayisal_puan,sinav_takip.esitagirlik_puan,sinav_takip.sozel_puan,veli_bilgileri.veli_adsoyad,veli_bilgileri.veli_telefon,veli_bilgileri.veli_mail from sinav_takip INNER JOIN veli_bilgileri ON sinav_takip.ogr_no = veli_bilgileri.ogr_no where sinav_takip.ogr_adsoyad like '%" + textBox1.Text + "%'";
+                GridDoldur(sqlsorgu);
+
+            }
+        }
+}
