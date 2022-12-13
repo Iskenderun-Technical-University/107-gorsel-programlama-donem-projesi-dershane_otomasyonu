6a867a90b2f437065005a744f89a52c9a4de08ae
musaacibalik <musa.acibalik.mdbf21@iste.edu.tr>
2022-12-13 12:56:47 +0300

    hesap bilgileri formunu yükledim

---
+++ 2022-2023-gorselodev/2022-2023-gorselodev/hesap_bilgileri.Designer.cs
@@ -0,0 +1,375 @@
+﻿
+namespace _2022_2023_gorselodev
+{
+    partial class hesap_bilgileri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.odemetarihi = new System.Windows.Forms.DateTimePicker();
+            this.datekayittarihi = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.odeyenkisi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.search2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.odenentutar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hesapogradsoyad = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ogrkayitucreti = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.hesapogrno = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btntemizle = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.btnekle = new System.Windows.Forms.Button();
+            this.btnguncelle = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(30, 288);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1049, 208);
+            this.groupBox3.TabIndex = 21;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kayıtlar";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1043, 179);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.odemetarihi);
+            this.groupBox2.Controls.Add(this.datekayittarihi);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.odeyenkisi);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.search2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.odenentutar);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.hesapogradsoyad);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.ogrkayitucreti);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.hesapogrno);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(41, 36);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(911, 218);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hesap Bilgileri";
+            // 
+            // odemetarihi
+            // 
+            this.odemetarihi.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.odemetarihi.Location = new System.Drawing.Point(527, 135);
+            this.odemetarihi.Name = "odemetarihi";
+            this.odemetarihi.Size = new System.Drawing.Size(273, 30);
+            this.odemetarihi.TabIndex = 12;
+            // 
+            // datekayittarihi
+            // 
+            this.datekayittarihi.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.datekayittarihi.Location = new System.Drawing.Point(122, 180);
+            this.datekayittarihi.Name = "datekayittarihi";
+            this.datekayittarihi.Size = new System.Drawing.Size(273, 30);
+            this.datekayittarihi.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(419, 189);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 23);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Ödeyen Kişi";
+            // 
+            // odeyenkisi
+            // 
+            this.odeyenkisi.Location = new System.Drawing.Point(527, 186);
+            this.odeyenkisi.Name = "odeyenkisi";
+            this.odeyenkisi.Size = new System.Drawing.Size(212, 30);
+            this.odeyenkisi.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(64, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Sıra No";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(184, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 30);
+            this.textBox1.TabIndex = 7;
+            // 
+            // search2
+            // 
+            this.search2.Location = new System.Drawing.Point(162, 287);
+            this.search2.Name = "search2";
+            this.search2.Size = new System.Drawing.Size(192, 30);
+            this.search2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ara";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(24, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 23);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Kayıt Tarihi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(27, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Öğrenci No";
+            // 
+            // odenentutar
+            // 
+            this.odenentutar.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.odenentutar.Location = new System.Drawing.Point(527, 86);
+            this.odenentutar.Name = "odenentutar";
+            this.odenentutar.Size = new System.Drawing.Size(212, 30);
+            this.odenentutar.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(10, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Öğrenci Ad Soyad";
+            // 
+            // hesapogradsoyad
+            // 
+            this.hesapogradsoyad.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.hesapogradsoyad.Location = new System.Drawing.Point(184, 126);
+            this.hesapogradsoyad.Name = "hesapogradsoyad";
+            this.hesapogradsoyad.Size = new System.Drawing.Size(192, 30);
+            this.hesapogradsoyad.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(419, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 23);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Kayıt Ücreti";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(405, 89);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 23);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Ödenen Tutar";
+            // 
+            // ogrkayitucreti
+            // 
+            this.ogrkayitucreti.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ogrkayitucreti.Location = new System.Drawing.Point(541, 33);
+            this.ogrkayitucreti.Name = "ogrkayitucreti";
+            this.ogrkayitucreti.Size = new System.Drawing.Size(212, 30);
+            this.ogrkayitucreti.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(405, 135);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 23);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Ödeme Tarihi";
+            // 
+            // hesapogrno
+            // 
+            this.hesapogrno.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.hesapogrno.Location = new System.Drawing.Point(184, 77);
+            this.hesapogrno.Name = "hesapogrno";
+            this.hesapogrno.Size = new System.Drawing.Size(192, 30);
+            this.hesapogrno.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btntemizle);
+            this.groupBox1.Controls.Add(this.btnsil);
+            this.groupBox1.Controls.Add(this.btnekle);
+            this.groupBox1.Controls.Add(this.btnguncelle);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(976, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 277);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İşlemler";
+            // 
+            // btntemizle
+            // 
+            this.btntemizle.Location = new System.Drawing.Point(42, 208);
+            this.btntemizle.Name = "btntemizle";
+            this.btntemizle.Size = new System.Drawing.Size(116, 39);
+            this.btntemizle.TabIndex = 6;
+            this.btntemizle.Text = "Temizle";
+            this.btntemizle.UseVisualStyleBackColor = true;
+            // 
+            // btnsil
+            // 
+            this.btnsil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsil.Location = new System.Drawing.Point(42, 155);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(116, 35);
+            this.btnsil.TabIndex = 5;
+            this.btnsil.Text = "Sil";
+            this.btnsil.UseVisualStyleBackColor = true;
+            // 
+            // btnekle
+            // 
+            this.btnekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnekle.Location = new System.Drawing.Point(42, 54);
+            this.btnekle.Name = "btnekle";
+            this.btnekle.Size = new System.Drawing.Size(116, 35);
+            this.btnekle.TabIndex = 5;
+            this.btnekle.Text = "Ekle";
+            this.btnekle.UseVisualStyleBackColor = true;
+            // 
+            // btnguncelle
+            // 
+            this.btnguncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnguncelle.Location = new System.Drawing.Point(42, 96);
+            this.btnguncelle.Name = "btnguncelle";
+            this.btnguncelle.Size = new System.Drawing.Size(116, 35);
+            this.btnguncelle.TabIndex = 5;
+            this.btnguncelle.Text = "Güncelle";
+            this.btnguncelle.UseVisualStyleBackColor = true;
+            // 
+            // hesap_bilgileri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1201, 511);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "hesap_bilgileri";
+            this.Text = "hesap_bilgileri";
+            this.Load += new System.EventHandler(this.hesap_bilgileri_Load);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker odemetarihi;
+        private System.Windows.Forms.DateTimePicker datekayittarihi;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox odeyenkisi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox search2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox odenentutar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox hesapogradsoyad;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox ogrkayitucreti;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox hesapogrno;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btntemizle;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.Button btnekle;
+        private System.Windows.Forms.Button btnguncelle;
+    }
+}
---
+++ 2022-2023-gorselodev/2022-2023-gorselodev/hesap_bilgileri.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2022_2023_gorselodev
+{
+    public partial class hesap_bilgileri : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+        SqlCommand cmd;
+        DataSet ds;
+        string SqlCon = Class1.SqlCon;
+        void GridDoldur()
+        {
+            con = new SqlConnection(SqlCon);
+            da = new SqlDataAdapter("Select * from hesap_bilgileri", con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "hesap_bilgileri");
+
+            dataGridView1.DataSource = ds.Tables["hesap_bilgileri"];
+            con.Close();
+        }
+        public hesap_bilgileri()
+        {
+            InitializeComponent();
+            if (Class1.BaglantiDurum())
+            {
+                // MessageBox.Show("Bağlantı Kuruldu");
+            }
+        }
+
+        private void hesap_bilgileri_Load(object sender, EventArgs e)
+        {
+            string veri = "select * from ogr_bilgileri where ogr_tcno='" + ogr_bilgileri.deger + "'";
+            hesapogrno.Text = Convert.ToString(Class1.IdDegeri(veri));
+            hesapogradsoyad.Text = ogr_bilgileri.deger_3;
+            datekayittarihi.Text = ogr_bilgileri.deger_4;
+            ogrkayitucreti.Text = veli_bilgileri.deger_5;
+
+            Class1.GridDoldur(dataGridView1, "select * from hesap_bilgileri ");
+            dataGridView1.Columns[0].HeaderCell.Value = "Sıra No";
+            dataGridView1.Columns[1].HeaderCell.Value = "Öğrenci No";
+            dataGridView1.Columns[2].HeaderCell.Value = "Öğrenci Ad Soyad";
+            dataGridView1.Columns[3].HeaderCell.Value = "Kayıt Tarihi";
+            dataGridView1.Columns[4].HeaderCell.Value = "Kayıt Ücreti";
+            dataGridView1.Columns[5].HeaderCell.Value = "Ödenen Tutar";
+            dataGridView1.Columns[6].HeaderCell.Value = "Ödeme Tarihi";
+            dataGridView1.Columns[7].HeaderCell.Value = "Ödeyen Kişi";
+        }
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secim = dataGridView1.SelectedCells[0].RowIndex;
+            textBox1.Text = dataGridView1.Rows[secim].Cells[0].Value.ToString();
+            hesapogrno.Text = dataGridView1.Rows[secim].Cells[1].Value.ToString();
+            hesapogradsoyad.Text = dataGridView1.Rows[secim].Cells[2].Value.ToString();
+            datekayittarihi.Text = dataGridView1.Rows[secim].Cells[3].Value.ToString();
+            ogrkayitucreti.Text = dataGridView1.Rows[secim].Cells[4].Value.ToString();
+            odenentutar.Text = dataGridView1.Rows[secim].Cells[5].Value.ToString();
+            odemetarihi.Text = dataGridView1.Rows[secim].Cells[6].Value.ToString();
+            odeyenkisi.Text = dataGridView1.Rows[secim].Cells[7].Value.ToString();
+
+        }
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            {
+                string sql1 = "DELETE FROM hesap_bilgileri WHERE odeme_id=@hesap_id";
+                string parametre = "@hesap_id";
+                string sql = "Select * from hesap_bilgileri";
+                foreach (DataGridViewRow drow in dataGridView1.SelectedRows)
+                {
+                    int id = Convert.ToInt32(drow.Cells[0].Value);
+                    Class1.GridView_Delete(id, sql1, parametre);
+                }
+                Class1.GridDoldur(dataGridView1, sql);
+            }
+        }
+    }
+}
