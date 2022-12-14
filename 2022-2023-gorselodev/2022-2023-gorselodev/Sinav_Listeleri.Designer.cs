addc95e9bd839b1b44b4093d1887d0c24cb614f3
gökhanunal01 <gokhanunal.mdbf20@iste.edu.tr>
2022-12-14 16:08:10 +0300

    sınav listesi formu ve kodlarını yükledim

---
+++ 2022-2023-gorselodev/2022-2023-gorselodev/Sinav_Listeleri.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace _2022_2023_gorselodev
+{
+    partial class Sinav_Listeleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 101);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(913, 360);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.radioButton4);
+            this.panel3.Controls.Add(this.radioButton3);
+            this.panel3.Controls.Add(this.radioButton2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Location = new System.Drawing.Point(1, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(913, 100);
+            this.panel3.TabIndex = 25;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(548, 41);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 43);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Yazdır";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.radioButton6);
+            this.panel4.Controls.Add(this.radioButton5);
+            this.panel4.Location = new System.Drawing.Point(94, 62);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(146, 36);
+            this.panel4.TabIndex = 32;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(83, 10);
+            this.radioButton6.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(57, 17);
+            this.radioButton6.TabIndex = 34;
+            this.radioButton6.Text = "Azalan";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(9, 10);
+            this.radioButton5.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(50, 17);
+            this.radioButton5.TabIndex = 33;
+            this.radioButton5.Text = "Artan";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(460, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 46);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton4.Location = new System.Drawing.Point(500, 5);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(132, 19);
+            this.radioButton4.TabIndex = 30;
+            this.radioButton4.Text = "Sözel Puanına Göre";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.Location = new System.Drawing.Point(328, 5);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(160, 19);
+            this.radioButton3.TabIndex = 29;
+            this.radioButton3.Text = "Eşit Ağırlık Puanına Göre";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(172, 5);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(138, 19);
+            this.radioButton2.TabIndex = 28;
+            this.radioButton2.Text = "Sayısal Puanına Göre";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(169, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Sınav No";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(245, 41);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 23);
+            this.textBox1.TabIndex = 25;
+            // 
+            // Sinav_Listeleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 454);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Sinav_Listeleri";
+            this.Text = "Sinav_Listeleri";
+            this.Load += new System.EventHandler(this.Sinav_Listeleri_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ 2022-2023-gorselodev/2022-2023-gorselodev/Sinav_Listeleri.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _2022_2023_gorselodev
+{
+    public partial class Sinav_Listeleri : Form
+    {
+        DataSet ds;
+
+        public Sinav_Listeleri()
+        {
+            InitializeComponent();
+        }
+        string SqlCon = Class1.SqlCon;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                if (radioButton5.Checked == true)
+                {
+                    string metin = "select * from sinav_takip where sinav_no='" + textBox1.Text + "' order by sayisal_puan asc";
+                    Class1.GridDoldur(dataGridView1, metin);
+                }
+                else if (radioButton6.Checked == true)
+                {
+                    string metin = "select * from sinav_takip where sinav_no='" + textBox1.Text + "' order by sayisal_puan desc";
+                    Class1.GridDoldur(dataGridView1, metin);
+                }
+                else
+                {
+                    MessageBox.Show("Artan veya azalan oldugunu seçiniz");
+                }
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[14].Visible = false;
+                dataGridView1.Columns[15].Visible = false;
+
+            }
+
+            else if (radioButton3.Checked == true)
+            {
+                if (radioButton5.Checked == true)
+                {
+                    string metin = "select * from sinav_takip where sinav_no='" + textBox1.Text + "' order by esitagirlik_puan asc";
+                    Class1.GridDoldur(dataGridView1, metin);
+                }
+                else if (radioButton6.Checked == true)
+                {
+                    string metin = "select * from sinav_takip where sinav_no='" + textBox1.Text + "' order by esitagirlik_puan desc";
+                    Class1.GridDoldur(dataGridView1, metin);
+                }
+                else
+                {
+                    MessageBox.Show("Artan veya azalan oldugunu seçiniz");
+                }
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[13].Visible = false;
+                dataGridView1.Columns[15].Visible = false;
+
+            }
+            else if (radioButton4.Checked == true)
+            {
+                if (radioButton5.Checked == true)
+                {
+                    string metin = "select * from sinav_takip where sinav_no='" + textBox1.Text + "' order by sozel_puan asc";
+                    Class1.GridDoldur(dataGridView1, metin);
+                }
+                else if (radioButton6.Checked == true)
+                {
+                    string metin = "select * from sinav_takip where sinav_no='" + textBox1.Text + "' order by sozel_puan desc";
+                    Class1.GridDoldur(dataGridView1, metin);
+                }
+                else
+                {
+                    MessageBox.Show("Artan veya azalan oldugunu seçiniz");
+                }
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[13].Visible = false;
+                dataGridView1.Columns[14].Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Arama Kriterlerini seçiniz");
+            }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                SinavRaporForm a = new SinavRaporForm();
+                a.kullanici = textBox1.Text;
+                a.ds = ds;
+                a.Show();
+
+            }
+        }
+    }
+}
